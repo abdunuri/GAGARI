a35e74074be12c292bba6bfc3cdc50490c7b69c8
Abdu <abdulaziznuri495@gmail.com>
2026-04-09 11:09:01 +0300

    order auto tested

--- src/services/script.ts
+++ src/services/script.ts
@@ -3,7 +3,7 @@ import { $Enums } from "@prisma/client";
 
 console.log('DATABASE_URL =', process.env.DATABASE_URL)
 
-async function main() {
+async function createUser() {
     const User = await prisma.user.create({
         data:{
             name:"Abdulber",
@@ -16,7 +16,20 @@ async function main() {
     const allUsers = await prisma.user.findMany();
     console.log(allUsers)
 }
-main().then (async() => { await prisma.$disconnect()})
+
+async function createOrder() {
+    const order = await prisma.order.create({
+        data:{
+            userId:1,
+            customerId :1   
+        }
+    }
+    )
+    console.log("order created",order);
+    const allorder = await prisma.order.findMany()
+    console.log(allorder)
+}
+createOrder().then (async() => { await prisma.$disconnect()})
     .catch(async(e) =>{
         console.log(e);
         await prisma.$disconnect();
