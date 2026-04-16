2e982594b7fbd416eaa8fa83189fc7f92093959a
Abdu <abdulaziznuri495@gmail.com>
2026-04-17 00:33:27 +0300

    ssl issue prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,13 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 const connectionString: string = process.env.DATABASE_URL as string;
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({
+	connectionString,
+	ssl:
+		process.env.NODE_ENV === "production"
+			? { rejectUnauthorized: false }
+			: undefined,
+});
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
