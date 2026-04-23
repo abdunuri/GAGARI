bf6281c06bd30e50bf8fca62e8fde63dbdb3bd40
Abdu <abdulaziznuri495@gmail.com>
2026-04-23 20:28:18 +0300

    new production db with new signup bootstrup logic

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 
-const PUBLIC_PATHS = ["/", "/login",];
+const PUBLIC_PATHS = ["/", "/login", "/signup"];
 
 export async function proxy(request: NextRequest) {
     const { pathname } = request.nextUrl;
