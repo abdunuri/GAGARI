3aeb1c2ea9aa500975395ae5c006d2f4edd69901
Abdu <abdulaziznuri495@gmail.com>
2026-04-26 14:25:45 +0300

    origin from new domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.125.32.209'],
+  allowedDevOrigins: ['10.125.32.209','www.gagari.app', 'gagari.app','www.gagari.vercel.app', 'gagari.vercel.app'],
   images: {
     remotePatterns: [
       {
